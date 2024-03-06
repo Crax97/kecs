364d7d75e98951dffe664c844c840ad1d5aec74c
Crax <gsolimeno97@gmail.com>
2024-03-06 13:44:04 +0100

    Refactor SparseSet to remove usize bounds

--- src/sparse_set.rs
+++ src/sparse_set.rs
@@ -1,15 +1,15 @@
-struct SparseKey<T> {
-    index: usize,
+struct SparseKey<I: SparseIndex, T> {
+    index: I,
     value: T,
 }
 
 /// Trait identifying all the objects that can be used as [`SparseSet] indexes
-pub trait SparseIndex: Default + From<usize> + Copy {
+pub trait SparseIndex: Default + Copy {
     /// Returns the key's unique index
     fn index(&self) -> usize;
 }
 
-impl<I: Default + Copy + Clone + Into<usize> + From<usize>> SparseIndex for I {
+impl<I: Default + Copy + Clone + Into<usize>> SparseIndex for I {
     fn index(&self) -> usize {
         (*self).into()
     }
@@ -17,21 +17,21 @@ impl<I: Default + Copy + Clone + Into<usize> + From<usize>> SparseIndex for I {
 
 /// A SparseSet is a data structure that can add, remove, index in constant time and iterate in linear time
 /// <https://skypjack.github.io/2019-03-07-ecs-baf-part-2/>
-pub struct SparseSet<I, T> {
-    dense: Vec<SparseKey<T>>,
-    sparse: Vec<I>,
+pub struct SparseSet<I: SparseIndex, T> {
+    dense: Vec<SparseKey<I, T>>,
+    sparse: Vec<usize>,
 }
 
-impl<T: Clone> Clone for SparseKey<T> {
+impl<I: SparseIndex + Clone, T: Clone> Clone for SparseKey<I, T> {
     fn clone(&self) -> Self {
         Self {
-            index: self.index,
+            index: self.index.clone(),
             value: self.value.clone(),
         }
     }
 }
 
-impl<T: Clone, I: Clone> Clone for SparseSet<T, I> {
+impl<T: SparseIndex, I: Clone> Clone for SparseSet<T, I> {
     fn clone(&self) -> Self {
         Self {
             dense: self.dense.clone(),
@@ -68,17 +68,17 @@ impl<I: SparseIndex, T> SparseSet<I, T> {
     /// Adds a new item to the sparse set
     /// Returns true if index was not present in the set, false otherwise
     pub fn insert(&mut self, index: I, value: T) -> bool {
-        let index = index.index();
-        if let Some(key) = self.get_key(index) {
+        let index_value = index.index();
+        if let Some(key) = self.get_key(index_value) {
             self.dense[key].value = value;
             false
         } else {
             let len = self.len();
             self.dense.push(SparseKey { index, value });
 
-            self.ensure_capacity_for(index);
+            self.ensure_capacity_for(index_value);
 
-            self.sparse[index] = len.into();
+            self.sparse[index_value] = len;
             true
         }
     }
@@ -104,13 +104,13 @@ impl<I: SparseIndex, T> SparseSet<I, T> {
     /// Returns `true` if the item with index `index` was present
     pub fn remove(&mut self, index: I) -> bool {
         if self.get(&index).is_some() {
-            let index = index.index();
+            let index_addr = index.index();
             let n = self.dense.len() - 1;
-            let index = self.sparse[index].index();
+            let index_addr = self.sparse[index_addr].index();
             let old_index = self.dense[n].index;
 
-            self.dense.swap(index, n);
-            self.sparse[old_index] = index.into();
+            self.dense.swap(index_addr, n);
+            self.sparse[old_index.index()] = index_addr;
             self.dense.pop();
             return true;
         }
@@ -119,7 +119,7 @@ impl<I: SparseIndex, T> SparseSet<I, T> {
 
     /// Iterates all the item in the sparse set, along with their values
     pub fn iter(&self) -> impl Iterator<Item = (I, &T)> {
-        self.dense.iter().map(|d| (I::from(d.index), &d.value))
+        self.dense.iter().map(|d| (d.index, &d.value))
     }
 
     /// Iterates mutably all the item in the sparse set, along with their values
@@ -140,7 +140,7 @@ impl<I: SparseIndex, T> SparseSet<I, T> {
         if key_index >= self.dense.len() {
             return None;
         }
-        if index == self.dense[key_index].index {
+        if index == self.dense[key_index].index.index() {
             Some(key_index)
         } else {
             None
@@ -149,7 +149,7 @@ impl<I: SparseIndex, T> SparseSet<I, T> {
 
     fn ensure_capacity_for(&mut self, index: usize) {
         if self.sparse.len() <= index {
-            self.sparse.resize(index + 1, I::default());
+            self.sparse.resize(index + 1, usize::MAX);
         }
     }
 
@@ -163,7 +163,7 @@ impl<I: SparseIndex, T> SparseSet<I, T> {
     }
 }
 
-impl<C: std::fmt::Debug> std::fmt::Debug for SparseKey<C> {
+impl<C: std::fmt::Debug, I: SparseIndex + std::fmt::Debug> std::fmt::Debug for SparseKey<I, C> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("SparseKey")
             .field("index", &self.index)
@@ -172,7 +172,7 @@ impl<C: std::fmt::Debug> std::fmt::Debug for SparseKey<C> {
     }
 }
 
-impl<C: std::fmt::Debug, I: std::fmt::Debug> std::fmt::Debug for SparseSet<C, I> {
+impl<C: std::fmt::Debug, I: std::fmt::Debug + SparseIndex> std::fmt::Debug for SparseSet<I, C> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("SparseSet")
             .field("dense", &self.dense)
